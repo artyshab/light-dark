f1ec98ec910dccd59dd6055bcd08ac263567771f
artyshab <156674256+artyshab@users.noreply.github.com>
2024-03-19 12:28:19 +0100

    test5

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let toggler = document.querySelector(".toggle");
+let navigationContent=document.querySelector(".navigation-content");
+let closer=document.querySelector(".x-close");
+
+
+
+toggler.addEventListener("click",()=>{
+    navigationContent.classList.add("navigation-content--show");
+})
+closer.addEventListener("click",()=>{
+    navigationContent.classList.remove("navigation-content--show");
+
+})
+
 
 document.querySelector(".slider-button").addEventListener("click",()=>{
     document.querySelector(".slider").classList.toggle("active");
