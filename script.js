bd5500c4f04c3986ba705c9c16956bfd79836c07
artyshab <156674256+artyshab@users.noreply.github.com>
2024-03-19 12:18:54 +0100

    test2

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-let toggler = document.querySelector(".toggle");
-let navigationContent=document.querySelector(".navigation-content");
-let closer=document.querySelector(".x-close");
 
-
-
-toggler.addEventListener("click",()=>{
-    navigationContent.classList.add("navigation-content--show");
-})
-closer.addEventListener("click",()=>{
-    navigationContent.classList.remove("navigation-content--show");
-
-})
 
 
 document.querySelector(".slider-button").addEventListener("click",()=>{
@@ -18,3 +6,10 @@ document.querySelector(".slider-button").addEventListener("click",()=>{
 });
 
 
+let toggler = document.querySelector(".card");
+let textContent = document.querySelector(".paragraph");
+let closer = document.querySelector("p");
+
+toggler.addEventListener("click", () => {
+    textContent.classList.toggle("paragraph--show");
+});
